302777c7f793d7c37767e12f5a0d36df607c93b7
MaheshMediboyina <MaheshMediboyina@users.noreply.github.com>
2019-05-31 22:57:43 +0530

    Revert "Added Updated Logic"
    This reverts commit 2c436586f4cbff8db36d9f62e62c864466346cd4.

--- src/main/java/com/miracle/ordering/controller/FeatureOrderingController.java
+++ src/main/java/com/miracle/ordering/controller/FeatureOrderingController.java
@@ -1,90 +1,24 @@
 package com.miracle.ordering.controller;
 
-import java.util.List;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.miracle.common.api.bean.Feature;
-import com.miracle.common.controller.APIMicroService;
-import com.miracle.common.utils.CommonUtil;
-import com.miracle.exception.GatewayServiceException;
-import com.miracle.ordering.bean.UnorderedFeaturesBean;
-import com.miracle.ordering.exception.OrderingErrorCode;
-import com.miracle.ordering.exception.OrderingException;
-
+import com.miracle.feature.response.FeatureJson;
+import com.miracle.feature.response.FeatureResponse;
 
 @RestController
-@RequestMapping("/masterBot/project")
-public class FeatureOrderingController extends APIMicroService {
-public static final Logger logger = LoggerFactory.getLogger(FeatureOrderingController.class);
-	
-	@PostMapping(value = {"/orderedFeatures"},consumes = {MediaType.APPLICATION_JSON_VALUE,MediaType.TEXT_PLAIN_VALUE})
-	@ResponseBody
-	public List<Feature> runService(@RequestBody UnorderedFeaturesBean unorderedFeaturesBean) throws Exception {
-		List<Feature> unorderedFeature = null;
-		try
-		{
-			String filter = getFilter(unorderedFeaturesBean);
-		    unorderedFeature =  unorderedFeaturesBean.getFeatures();
-			CommonUtil.sortBeans(unorderedFeature, filter);
-			logger.info("Filtered "+unorderedFeaturesBean + " features based on the "+ filter + " . Filtered bean "+unorderedFeature);
-		}catch(OrderingException orderingException){
-			logger.error("Getting exception in applying fliter on features for feature Ordering, Exception Description :: "+orderingException.getMessage(),orderingException);
-			throw orderingException;
-		}catch(GatewayServiceException gatewayServiceException){
-			logger.error("Getting exception in applying fliter on features for feature Ordering, Exception Description :: "+gatewayServiceException.getMessage(),gatewayServiceException);
-			throw gatewayServiceException;
-		}
-		catch (Exception exception) {
-			logger.error("Getting exception in applying fliter on features for feature Ordering, Exception Description :: "+exception.getMessage(),exception);
-			throw new OrderingException("Getting exception in applying fliter on features  for feature Ordering, Exception Description :: "+exception.getMessage(),
-					exception,OrderingErrorCode.FEATURE_ORDERING_CONTROLLER_UNKNOWN_EXCEPTION,HttpStatus.INTERNAL_SERVER_ERROR);
-		}		
-		return unorderedFeature;
+public class FeatureOrderingController {
+
+	@GetMapping(value = "/orderedFeatures")
+	public ResponseEntity<FeatureResponse> buildFeatures(@RequestParam(value = "filter") String filter,
+			@RequestParam(value = "featureJsonList") FeatureJson featureJson) {
+		// Order features based on filter type
+		FeatureResponse response = new FeatureResponse();
+		// send ordered FeatureJson object using FeatureResponse
+		return new ResponseEntity<FeatureResponse>(response, HttpStatus.OK);
 	}
-	/**
-	 * 
-	 * @param unorderedFeaturesBean
-	 * @return
-	 * @throws OrderingException
-	 */
-//	private String getFilter(UnorderedFeaturesBean unorderedFeaturesBean)throws OrderingException
-//	{
-//		String filter ="";
-//		if( unorderedFeaturesBean.getFilterType()!= null && !unorderedFeaturesBean.getFilterType().isEmpty()) {
-//			filter = unorderedFeaturesBean.getFilterType();
-//		}
-//		else {
-//			logger.error("Invalid filtername in the request");
-//			throw new OrderingException("Invalid filtername in the request",OrderingErrorCode.INVALID_FILTER_TYPE,HttpStatus.BAD_REQUEST);
-//		}
-//		return filter;
-//	}
-	
-	private String getFilter(UnorderedFeaturesBean unorderedFeaturesBean)throws OrderingException
-	{
-		String filter ="";
-		try {
-			if( unorderedFeaturesBean.getFilterType()!= null && !unorderedFeaturesBean.getFilterType().isEmpty()) {
-				filter = mongoDBUtility.getFilterType(Integer.parseInt(unorderedFeaturesBean.getFilterType()));
-			}
-			else {
-				logger.error("Invalid filtername in the request");
-				throw new OrderingException("Invalid filtername in the request",OrderingErrorCode.INVALID_FILTER_TYPE,HttpStatus.BAD_REQUEST);
-			}
-		}catch (Exception exception) {
-			throw new OrderingException("Getting exception in applying fliter on features  for feature Ordering, Exception Description :: "+exception.getMessage(),
-					exception,OrderingErrorCode.INVALID_FILTER_TYPE,HttpStatus.INTERNAL_SERVER_ERROR);
-		}
-		
-		return filter;
-	}
+
 }
--- src/main/java/com/miracle/ordering/exception/OrderingErrorCode.java
+++ src/main/java/com/miracle/ordering/exception/OrderingErrorCode.java
@@ -1,6 +1,5 @@
 package com.miracle.ordering.exception;
 
-public interface OrderingErrorCode {
-	String INVALID_FILTER_TYPE = "MSB.ORD.IFT.000";
-	String FEATURE_ORDERING_CONTROLLER_UNKNOWN_EXCEPTION = "MSB.ORD.UN.000";
+public class OrderingErrorCode {
+
 }
